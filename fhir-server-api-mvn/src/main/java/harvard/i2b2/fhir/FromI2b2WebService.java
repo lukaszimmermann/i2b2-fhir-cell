4ed8a43a31da7c59f905940bfa16ad26d64e2c74
waghsk <waghsbox@gmail.com>
2015-04-02 15:10:28 -0400

    gets meds and transforms (TODO: select PDO for cohort id)

--- fhir-server-api-mvn/src/main/java/harvard/i2b2/fhir/FromI2b2WebService.java
+++ fhir-server-api-mvn/src/main/java/harvard/i2b2/fhir/FromI2b2WebService.java
@@ -1,12 +1,14 @@
 package harvard.i2b2.fhir;
 
 import harvard.i2b2.fhir.ejb.ResourceDb;
+import harvard.i2b2.fhir.ejb.XQueryProcessor;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.net.URI;
@@ -32,6 +34,7 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
@@ -46,6 +49,9 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.namespace.QName;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.xquery.XQConnection;
@@ -77,6 +83,9 @@ import org.xml.sax.InputSource;
 public class FromI2b2WebService {
 	//Logger logger= LoggerFactory.getLogger(ResourceFromI2b2WebService.class);
 	
+	@EJB
+	XQueryProcessor xqp;
+	
 	@javax.ws.rs.core.Context
 	ServletContext context;
 
@@ -84,7 +93,8 @@ public class FromI2b2WebService {
 	private void init() {
 		System.out.println("will run xquery");
 		try{
-			System.out.println(processXquery("hi"));
+			String input=getFile("example/i2b2/i2b2medspod.txt");
+			//System.out.println(processXquery(input));
 		}catch(Exception e){
 			e.printStackTrace();
 		}
@@ -93,108 +103,33 @@ public class FromI2b2WebService {
 	@GET
 	@Path("medication")
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public Response getMedsForPatientSet(
+	public String getMedsForPatientSet(
 			//@PathParam("patientId") String patientId,
 			@HeaderParam("accept") String acceptHeader) throws IOException {
 			//logger.log(Level.WARNING, "customer is null.");
 			Client client = ClientBuilder.newClient();
-			 Builder builder=
-			client.target("http://services.i2b2.org:9090/i2b2/services/QueryToolService/pdorequest")
-			.request(MediaType.APPLICATION_XML);
+			WebTarget myResource = client.target("http://services.i2b2.org:9090/i2b2/services/QueryToolService/pdorequest");
 			 String str=getFile("i2b2RequestMeds1.xml");
-			 Response response= builder.post(Entity.entity(str, MediaType.APPLICATION_XML));
-			System.out.println(response.toString());
-			return response; 		
+			 String oStr= myResource.request(MediaType.APPLICATION_XML).post(Entity.entity(str, MediaType.APPLICATION_XML),String.class);
+			 System.out.println("got:"+oStr.substring(0,(oStr.length()>200)?200:0));
+			 try{
+				 //String input=getFile("example/i2b2/i2b2medspod.txt");
+				 //str=processXquery(input);
+				 return processXquery(oStr.toString());
+			 }catch(Exception e){
+				 e.printStackTrace();
+			 }
+			 //System.out.println(str);
+			return null; 		
 	}
 	
-	private String processXquery(String input) throws InstantiationException, IllegalAccessException, ClassNotFoundException, XQException{
-		XQDataSource xqds = new SaxonXQDataSource();
-		XQConnection conn = xqds.getConnection();
-		// The XQuery expression to be executed
-		 final String sep = System.getProperty("line.separator");
-		String query = "declare variable $x as xs:integer external; "
-						+sep+" for $n in $x"+
-						sep +" return  <test>$n+1</test>";
-		 
-		// Bind a value (21) to an external variable with the QName x
-		 
-		// Execute the XQuery expression
-		//XQExpression expr = conn.createExpression();
-		 
-		XQPreparedExpression expr = conn.prepareExpression(query); 
-		expr.bindInt(new QName("x"), 21, null);
-		
-		
-		XQSequence result = expr.executeQuery();
-        //System.out.println(result.getSequenceAsString(null));
-        
-		return result.getSequenceAsString(null);
+	private String processXquery(String input) throws InstantiationException, IllegalAccessException, ClassNotFoundException, XQException, XMLStreamException{
+		String query = getFile("transform/I2b2ToFhir/i2b2MedsToFHIRMeds.xquery");
+	    return xqp.processXquery(query, input);
 	}
 	
 	
-	/*
-	protected String doGet(String input,String xquery) {
-		try {
-			StringWriter sw= new StringWriter();
-			 Processor processor1 = new Processor(false);
-			 //processor1.newSerializer(file)
-			 //processor1.
-			 Processor processor = new Processor(false);
-	       
-	        XQueryCompiler compiler = processor.newXQueryCompiler();
-	        Serializer serializer = new Serializer();
-	        serializer.setOutputWriter(sw);
-	        
-	        XQueryExecutable executable = compiler.compile(IOUtils.toInputStream(input));
-	        XQueryEvaluator evaluator = executable.load();
-	        evaluator.setDestination(serializer);
-	        evaluator.run();
-	        return sw.toString();
-	    } catch (SaxonApiException e) {
-	        e.printStackTrace();
-	    }
-	    return null;
-	}
 	
-	public void go(String xmldata, String queryfilename) throws Exception {
-		//
-		// Get the input query file
-		FileInputStream fin = new FileInputStream(queryfilename);
-		BufferedReader newquery
-		= new BufferedReader(new InputStreamReader(fin));
-		// We could make the query a string, for example
-		String squery = "for $a in fn:distinct-values(//author)" +
-		"return (xs:string($a)," +
-		"for $b in //book[author = $a]" +
-		"return $b/title)";
-		
-		// Now get the XML file wiht the and make it a SAX Source
-		InputSource is = new InputSource(new File(xmlfilename).toURL().toString()); 
-		SAXSource saxsource = new SAXSource( is);
-		
-		StringReader stringread = new StringReader( xmldata);
-		StreamSource streamsource = new StreamSource( stringread);
-		// if you want the string xmldata replace saxsource below with streamsource 
-		Configuration config = new Configuration();
-		config.setHostLanguage( config.XQUERY);
-		
-		StaticQueryContext staticQueryContext = new StaticQueryContext();
-		QueryProcessor qp = new QueryProcessor(config, staticQueryContext);
-		DocumentInfo docinfo = qp.buildDocument( saxsource);
-		XQueryExpression exp = qp.compileQuery( newquery);
-		DynamicQueryContext dynamicQueryContext = new DynamicQueryContext();
-		dynamicQueryContext.setContextNode( docinfo);
-		// evaluate the query and get a java.util.List
-		List querylist = exp.evaluate(dynamicQueryContext);
-		// now get a java.util iterator
-		Iterator javautilIter = querylist.iterator();
-		// now evaluate the query and get a net.sf.saxon.om.SequenceIterator
-		SequenceIterator saxonIter = exp.iterator(dynamicQueryContext);
-		// Output the results
-		System.out.println("Number Nodes = " + querylist.size());
-		printout( javautilIter, saxonIter);
-		}
-*/	
 	private String getFile(String fileName){
 		 
 		  String result = "";
