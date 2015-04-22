72adec7260909a9645e3b110730283c7f52aa757
waghsk <waghsbox@gmail.com>
2015-04-22 15:10:58 -0400

    works

--- xjctestmvn/src/test/java/example/jaxbtutorial1.java
+++ xjctestmvn/src/test/java/example/jaxbtutorial1.java
@@ -6,16 +6,23 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
-import org.hl7.fhir.FhirResourceWithMD;
 import org.hl7.fhir.Medication;
-import org.hl7.fhir.MetaData;
 import org.junit.Test;
+
+import edu.harvar.i2b2.fhir.core.MetaData;
+import edu.harvar.i2b2.fhir.core.MetaResource;
+import edu.harvar.i2b2.fhir.core.MetaResourceSet;
+
 
 
 
@@ -23,19 +30,22 @@ public class jaxbtutorial1 {
 
 	
 	@Test
-	public void test2() throws JAXBException, IOException{
+	public void test2() throws JAXBException, IOException, DatatypeConfigurationException{
 		 final String BOOKSTORE_XML = "./tmp.xml";
 
 		   
-
-		    ArrayList<FhirResourceWithMD> list = new ArrayList<FhirResourceWithMD>();
+		 MetaResourceSet s1= new MetaResourceSet ();
+		    ArrayList<MetaResource> list = new ArrayList<MetaResource>();
 
 		    // create books
 		    
-		    FhirResourceWithMD r1= new FhirResourceWithMD();
+		    MetaResource r1= new MetaResource();
 		    MetaData md1= new MetaData();
 		    md1.setId("medication/1");
-		    md1.setLastUpdated("2015-04-22");
+		    GregorianCalendar gc = new GregorianCalendar();
+	       
+
+		    md1.setLastUpdated( DatatypeFactory.newInstance().newXMLGregorianCalendar(gc));
 		    
 		    Medication m1= new Medication();
 		    org.hl7.fhir.String str1=new org.hl7.fhir.String();
@@ -46,15 +56,16 @@ public class jaxbtutorial1 {
 		    r1.setResource(m1);
 		    list.add(r1);
 		    
+		    
 		 
 		    // create JAXB context and instantiate marshaller
-		    JAXBContext context = JAXBContext.newInstance( FhirResourceWithMD.class);
+		    JAXBContext context = JAXBContext.newInstance( MetaResource.class);
 		    Marshaller m = context.createMarshaller();
 		    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
 		    // Write to System.out
 		    m.marshal(r1, System.out);
-
+/*
 		    // Write to File
 		    m.marshal(r1, new File(BOOKSTORE_XML));
 
@@ -64,7 +75,9 @@ public class jaxbtutorial1 {
 		    System.out.println("Output from our XML File: ");
 		    Unmarshaller um = context.createUnmarshaller();
 		   FhirResourceWithMD r2= (FhirResourceWithMD) um.unmarshal(new FileReader(BOOKSTORE_XML));
-		      System.out.println("FhirResourceWithMD: " +r2.getMetaData().getId()+"\n"+
+		      System.out.println("FhirResourceWithMD: " +((MetaData)r2.getMetaData()).getId()+"\n"+
 		    		  ((Medication)r2.getResource()).getName().getValue());
-		    }	
+	*/	    }	
+	
+	
 }
