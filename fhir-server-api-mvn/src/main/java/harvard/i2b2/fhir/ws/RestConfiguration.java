656528d6b9aa131cad2b903b2ade674a0d9a2adc
waghsk <waghsbox@gmail.com>
2015-05-04 17:39:20 -0400

    can the resource url be shortended

--- fhir-server-api-mvn/src/main/java/harvard/i2b2/fhir/ws/RestConfiguration.java
+++ fhir-server-api-mvn/src/main/java/harvard/i2b2/fhir/ws/RestConfiguration.java
@@ -3,7 +3,7 @@ package harvard.i2b2.fhir.ws;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("resources")
+@ApplicationPath("a")
 public class RestConfiguration extends Application {
 
 }
