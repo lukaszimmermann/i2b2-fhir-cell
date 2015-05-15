a556422c0acdbd0c235f5f200921309d548c8094
waghsk <waghsbox@gmail.com>
2015-05-15 19:20:45 -0400

    date matching works

--- i2b2-fhir/fhir-core/src/main/java/edu/harvard/i2b2/fhir/query/Query.java
+++ i2b2-fhir/fhir-core/src/main/java/edu/harvard/i2b2/fhir/query/Query.java
@@ -3,68 +3,93 @@ package edu.harvard.i2b2.fhir.query;
 import java.util.ArrayList;
 
 import org.hl7.fhir.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import edu.harvard.i2b2.fhir.FhirUtil;
+import edu.harvard.i2b2.fhir.XQueryUtil;
 import edu.harvard.i2b2.fhir.core.MetaResource;
 import edu.harvard.i2b2.fhir.core.MetaResourceSet;
 
 //parent to types of query, which implement the different search types
 public abstract class Query {
+	static Logger logger = LoggerFactory.getLogger(Query.class);
+
 	String parameter;
 	String value;
 	QueryType type;
 	String parameterPath;// this will be resource specific defined at runTime
 	Class resourceClass;
-	
-	private Query(){}
-	
-	public Query(Class resourceClass,String parameter, String value) throws QueryParameterException,QueryValueException{
-		this.resourceClass=resourceClass;
-		this.parameter=parameter;
-		this.value=value;
-		SearchParameterTuple t=SearchParameterTupleMap.getTuple(this.resourceClass, this.parameter);
-		if(t!=null)this.parameterPath=t.getPath();
+
+	private Query() {
+	}
+
+	public Query(Class resourceClass, String parameter, String value)
+			throws QueryParameterException, QueryValueException {
+		this.resourceClass = resourceClass;
+		this.parameter = parameter;
+		this.value = value;
+		SearchParameterTuple t = SearchParameterTupleMap.getTuple(
+				this.resourceClass, this.parameter);
+		if (t != null)
+			this.parameterPath = t.getPath();
 		validateParameter();
 		validateValue();
 		validateParameterPath();
-			
+
 	}
-	
-	
+
 	private void validateParameterPath() throws QueryParameterException {
-		if(this.parameterPath==null) throw new QueryParameterException(
-				"parameter Path not found for "+this.parameter+ " for class "+this.resourceClass);
+		if (this.parameterPath == null)
+			throw new QueryParameterException("parameter Path not found for "
+					+ this.parameter + " for class " + this.resourceClass);
 	}
 
 	abstract public boolean match(Resource r);
 
-	final public boolean match(MetaResource mr){
+	final public boolean match(MetaResource mr) {
 		return match(mr.getResource());
-		
+
 	}
-	
-	final public void removeNonMatching(MetaResourceSet s){
-		MetaResourceSet s2=new MetaResourceSet();
-		ArrayList<MetaResource> list= (ArrayList<MetaResource>) s2.getMetaResource();
-		ArrayList<MetaResource> slist=(ArrayList<MetaResource>) s.getMetaResource();
-		for(MetaResource mr:slist){
-			if(match(mr)) list.add(mr);
+
+	final public void removeNonMatching(MetaResourceSet s) {
+		MetaResourceSet s2 = new MetaResourceSet();
+		ArrayList<MetaResource> list = (ArrayList<MetaResource>) s2
+				.getMetaResource();
+		ArrayList<MetaResource> slist = (ArrayList<MetaResource>) s
+				.getMetaResource();
+		for (MetaResource mr : slist) {
+			if (match(mr))
+				list.add(mr);
 		}
 		slist.clear();
 		slist.addAll(list);
 	}
-	
+
 	abstract public void validateParameter() throws QueryParameterException;
-	
+
 	abstract public void validateValue() throws QueryValueException;
-	
-	public String toString(){
-		return "resourceClass="+this.resourceClass.getCanonicalName()
-				+"\nparameter="+this.parameter
-				+"\nvalue="+this.value
-				+"\ntype="+this.type
-				+"\nparameterPath"+this.parameterPath;
+
+	public String toString() {
+		return "resourceClass=" + this.resourceClass.getCanonicalName()
+				+ "\nparameter=" + this.parameter + "\nvalue=" + this.value
+				+ "\ntype=" + this.type + "\nparameterPath"
+				+ this.parameterPath;
 	}
-	
-	
+
+	// perhaps xquery might be faster than Java reflection, as the latter may
+	// involve making call for
+	// elements that may not be initialized
+	public ArrayList<String> getValuesBelowParameterPath(Resource r,
+			String parPath) {
+		ArrayList<String> list = new ArrayList<String>();
+		
+		String xml = FhirUtil.resourceToXml(r);
+		list = XQueryUtil.getStringSequence(xml,
+				"declare default element namespace \"http://hl7.org/fhir\";"
+						+ "/"+this.parameterPath.replace(".", "/")//"/Patient/birthDate+"
+						+ "/@value/string()");
+		logger.trace("list:"+list.toString());
+		return list;
+	}
 }
--- i2b2-fhir/fhir-core/src/test/java/edu/harvard/i2b2/fhir/query/QueryDateTest.java
+++ i2b2-fhir/fhir-core/src/test/java/edu/harvard/i2b2/fhir/query/QueryDateTest.java
@@ -2,10 +2,14 @@ package edu.harvard.i2b2.fhir.query;
 
 import static org.junit.Assert.*;
 
+import org.apache.commons.io.FileUtils;
 import org.hl7.fhir.Patient;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import edu.harvard.i2b2.fhir.FhirUtil;
+import edu.harvard.i2b2.fhir.Utils;
 
 public class QueryDateTest {
 
@@ -25,9 +29,25 @@ public class QueryDateTest {
 			assert(true);
 		}
 		
-		Query q=qb.setResourceClass(Patient.class).setParameter("birthdate").setValue("2015-05-15").build();
 		
-		logger.info(""+q);
+		String xml=Utils.getFile("example/fhir/singlePatient.xml");
+		Patient p=(Patient) FhirUtil.xmlToResource(xml);
+		
+		Query q=qb.setResourceClass(Patient.class).setParameter("birthdate").setValue("<2015-05-15").build();
+		logger.trace("RES:"+q.match(p));
+		assertTrue(q.match(p));
+		q=qb.setResourceClass(Patient.class).setParameter("birthdate").setValue(">1941-05-15").build();
+		logger.trace("RES:"+q.match(p));
+		assertTrue(q.match(p));
+		q=qb.setResourceClass(Patient.class).setParameter("birthdate").setValue("<1941-05-15").build();
+		logger.trace("RES:"+q.match(p));
+		assertFalse(q.match(p));
+		q=qb.setResourceClass(Patient.class).setParameter("birthdate").setValue("=<1944-11-17").build();
+		logger.trace("RES:"+q.match(p));
+		assertTrue(q.match(p));
+		
+		
+		
 	}
 
 }
