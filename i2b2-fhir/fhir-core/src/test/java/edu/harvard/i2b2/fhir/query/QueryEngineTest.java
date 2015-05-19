fc119d1f3694051b9bfb6689b104c31dfe4bafa9
waghsk <waghsbox@gmail.com>
2015-05-19 13:52:18 -0400

    working on creating query engine

--- i2b2-fhir/fhir-core/src/test/java/edu/harvard/i2b2/fhir/query/QueryEngineTest.java
+++ i2b2-fhir/fhir-core/src/test/java/edu/harvard/i2b2/fhir/query/QueryEngineTest.java
@@ -9,16 +9,16 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+
 import org.hl7.fhir.Patient;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import edu.harvard.i2b2.fhir.FhirUtil;
+import edu.harvard.i2b2.fhir.SetupExamples;
 import edu.harvard.i2b2.fhir.Utils;
 import edu.harvard.i2b2.fhir.core.FhirCoreException;
 import edu.harvard.i2b2.fhir.core.MetaResource;
@@ -27,26 +27,38 @@ import edu.harvard.i2b2.fhir.core.MetaResourceSet;
 public class QueryEngineTest {
 	static Logger logger = LoggerFactory.getLogger(QueryEngineTest.class);
 	Patient p;
+	Patient p2;
+
 	QueryEngine qe;
-	
+
 	@Before
 	public void setup() {
 		String xml = Utils.getFile("example/fhir/singlePatient.xml");
 		p = (Patient) FhirUtil.xmlToResource(xml);
+		xml = Utils
+				.getFile("example/fhir/singlePatientWithoutCodeSystemForGender.xml");
+		p2 = (Patient) FhirUtil.xmlToResource(xml);
 	}
 
 	@Test
-	public void testQueryUrl() throws QueryParameterException, QueryValueException, FhirCoreException  {
-		qe=new QueryEngine("Patient?name=pieter&gender=M");
+	public void testQueryUrl() throws QueryParameterException,
+			QueryValueException, FhirCoreException, DatatypeConfigurationException {
+		qe = new QueryEngine("Patient?name=pieter&gender=M");
 		logger.trace(qe.toString());
-		List<Resource> resourceList=new ArrayList<Resource>();
+		List<Resource> resourceList = new ArrayList<Resource>();
 		resourceList.add(p);
 		qe.search(resourceList);
-		
-		MetaResourceSet s=new MetaResourceSet();
-		MetaResource mr=FhirUtil.getMetaResource(p);
+
+		MetaResourceSet s = new MetaResourceSet();
+		MetaResource mr = FhirUtil.getMetaResource(p);
 		s.getMetaResource().add(mr);
-		
-		qe.search(s);
+		MetaResource mr2 = FhirUtil.getMetaResource(p2);
+		s.getMetaResource().add(mr2);
+		s.getMetaResource().add(SetupExamples.getEGPatient().getMetaResource().get(0));
+
+
+		MetaResourceSet s2=qe.search(s);
+		logger.trace("Input:"+s.getMetaResource().size());
+		logger.trace("Result:"+s2.getMetaResource().size());
 	}
 }
