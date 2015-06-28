21d8d9f8c6d6dcbd23465b6febe02dca3aa2a8b7
waghsk <waghsbox@gmail.com>
2015-06-27 23:05:40 -0400

    resteady works

---
+++ i2b2-fhir/restws-jboss7/src/main/java/Login.java
@@ -0,0 +1,16 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path( "/test" )
+public class Login {
+
+   @GET
+   //@Produces(MediaType.TEXT_HTML)
+   public Response login()  {
+      return Response.ok( "logged in" ).build();
+   }
+
+}
