12c2a7ff97e4f0a7552875795422196bfdd4b6ee
waghsk <waghsbox@gmail.com>
2015-07-02 16:36:57 -0400

    deleted unecessary files

--- fhir-server-api/src/main/java/edu/harvard/i2b2/fhirServerApi/jba/entity/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.harvard.i2b2.fhirServerApi.jba.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Integer>{
-
-}
