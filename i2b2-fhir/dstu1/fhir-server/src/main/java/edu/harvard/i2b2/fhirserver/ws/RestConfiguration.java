b04a53b144cb971aa9ebe78c210b74ae2f4b6d25
waghsk <waghsbox@gmail.com>
2015-07-07 19:07:13 -0400

    Patients works on JBOSS7. but got to correct MRS for presription. and also do logging

--- i2b2-fhir/dstu1/fhir-server/src/main/java/edu/harvard/i2b2/fhirserver/ws/RestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package edu.harvard.i2b2.fhirserver.ws;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("service")
-public class RestConfiguration extends Application {
-
-}
