60f8175f457b6caff1d637781d67400e9381d518
waghsk <waghsbox@gmail.com>
2015-07-31 17:06:58 -0400

    to direct FileServlet to point to base directory, instead of using demo

--- i2b2-fhir/dstu1/restws-1/src/main/java/edu/harvard/i2b2/fhirserver/ws/OAuth2AuthzEndpoint.java
+++ i2b2-fhir/dstu1/restws-1/src/main/java/edu/harvard/i2b2/fhirserver/ws/OAuth2AuthzEndpoint.java
@@ -16,6 +16,7 @@ import java.net.URISyntaxException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
@@ -36,6 +37,23 @@ import org.slf4j.LoggerFactory;
 
 /*
  * http://blogs.steeplesoft.com/posts/2013/a-simple-oauth2-client-and-server-example-part-i.html
+ */
+
+/*
+ * The authorization server validates the request to ensure that all
+   required parameters are present and valid.  The authorization server
+   MUST verify that the redirection URI to which it will redirect the
+   access token matches a redirection URI registered by the client as
+   described in Section 3.1.2.
+
+   If the request is valid, the authorization server authenticates the
+   resource owner and obtains an authorization decision (by asking the
+   resource owner or by establishing approval via other means).
+
+   When a decision is established, the authorization server directs the
+   user-agent to the provided client redirection URI using an HTTP
+   redirection response, or by other means available to it via the
+   user-agent.
  */
 @Path("authz")
 public class OAuth2AuthzEndpoint {
@@ -59,6 +77,8 @@ public class OAuth2AuthzEndpoint {
                     OAuthASResponse.authorizationResponse(request,
                         HttpServletResponse.SC_FOUND);
 
+            request.getAttribute("client_id");
+            
             // 1
             if (responseType.equals(ResponseType.CODE.toString())) {
                 final String authorizationCode =
@@ -82,5 +102,11 @@ public class OAuth2AuthzEndpoint {
         	return Response.status(Status.BAD_REQUEST)
 					.header("xreason", e.getMessage()).build();
         }
+    }
+    
+    private HttpSession getSessionWithAuthorizationCode(HttpServletRequest request,String authorizationCode){
+    	HttpSession session = request.getSession(false);
+    	session.setAttribute("AuthorizationCode", authorizationCode);
+    	return session;
     }
 }
