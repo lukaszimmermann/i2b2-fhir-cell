29369fdb5590bd37d7da9f6ee53337a32ac9693e
waghsk <waghsbox@gmail.com>
2015-08-21 13:58:53 -0400

    for HAPI to correctly translate xml to json appended #- to reference and removed slashes for the id of content

---
+++ i2b2-fhir/dstu2/testsrv-2/src/main/java/edu/harvard/i2b2/testfhirserver/ws/SearchUrlFilter.java
@@ -0,0 +1,50 @@
+package edu.harvard.i2b2.testfhirserver.ws;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.ext.ReaderInterceptor;
+import javax.ws.rs.ext.ReaderInterceptorContext;
+import javax.ws.rs.ext.WriterInterceptor;
+import javax.ws.rs.ext.WriterInterceptorContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.harvard.i2b2.fhir.FhirUtil;
+
+@Provider
+@PreMatching
+public class SearchUrlFilter implements ContainerRequestFilter {
+	static Logger logger = LoggerFactory.getLogger(SearchUrlFilter .class);
+	
+ 		@Override
+	public void filter(ContainerRequestContext context) throws IOException {
+ 			
+ 			UriInfo uriInfo=context.getUriInfo();
+		String pathStr=uriInfo.getAbsolutePath().toString();
+		logger.info("got uri::"+pathStr);
+		pathStr=pathStr.replace("/_search", "");
+	//	pathStr="http://localhost:8080/srv-dstu1-0.2/api/Patient";
+		URI uri=null;
+		try {
+			uri = new URI(pathStr);
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			logger.equals(e);
+			e.printStackTrace();
+			
+		}
+		logger.info("rewriting search url to:"+uri.toString());
+		context.setRequestUri(uri);
+		
+	}
+
+}
