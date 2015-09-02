a96e9e8fe366bad6bec0e936d14098b356724792
waghsk <waghsbox@gmail.com>
2015-09-02 13:18:30 -0400

    working on install script

--- i2b2-fhir/i2b2-fhir-install.sh
+++ i2b2-fhir/i2b2-fhir-install.sh
@@ -13,52 +13,72 @@ echo "DEPLOY_DIR:$DEPLOY_DIR"
 #export PATH="$PATH;$MAVEN_HOME/bin;$JAVA_HOME/bin"
 #echo "PATH:$PATH"
 
-export MVN="$MAVEN_HOME/bin/mvn"
-echo "MVN:$MVN" 
 export java="$JAVA_HOME/bin/java"
 echo "java:$java" 
 
-if [ "1" -eq "1" ]; then echo "hi"
+if [ "2" -eq "1" ]; then echo "hi"
+fi
 
 #check if java is installed
 
-$(java 2>/dev/null );if [ $? == 0 ];  then echo "Java was not found. Please install java 1.7 or higher"; fi
+$(java 2>/dev/null )
+if [ $? == 0 ]
+  then echo "Java was not found. Please install java 1.7 or higher";
+fi
+
 JAVA_VER=$(java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q');
 echo "java version:$JAVA_VER" 
 
-if [ $JAVA_VER -lt 17 ]; then echo " Java 1.7 or higher is required" ;fi
-wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.tar.gz
-tar -xzf jdk-7u79-linux-x64.tar.gz
-
+#if [ $JAVA_VER -lt 17 ]; then echo " Java 1.7 or higher is required. Installing .."
+if [ -f jdk-7u79-linux-x64.tar.gz ] 
+then echo ""
+else
+	wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.tar.gz
+	tar -xzf jdk-7u79-linux-x64.tar.gz
+fi
 
 #check if maven is installed
 $(mvn 1>/dev/null );
 #echo $?
-if [ $? == 0 ];  then echo "mvn was not found. Installing maven"; 
+if [ $? == 0 ]
+  then echo "mvn was not found. Installing maven" 
 
-wget http://apache.mirrors.ionfish.org/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz
-tar -xzf apache-maven-3.3.3-bin.tar.gz
+	if [ -f apache-maven-3.3.3-bin.tar.gz ] 
+	then echo ""
+	else
 
-export MAVEN_HOME="$(pwd)/apache-maven-3.3.3"
-echo "MAVEN_HOME=$MAVEN_HOME"
+		wget http://apache.mirrors.ionfish.org/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz
+		tar -xzf apache-maven-3.3.3-bin.tar.gz
+	fi
 
+	export MAVEN_HOME="$(pwd)/apache-maven-3.3.3"
+	echo "MAVEN_HOME=$MAVEN_HOME"
+	export MVN="$MAVEN_HOME/bin/mvn"
+	echo "MVN:$MVN" 
+
+fi
 #$MVN -version 1>/dev/null  
 #echo $?
 #if [ $? -ne 0 ];  then echo "mvn was not found. Please install maven  or higher"; fi
 
+
+if [ -f wildfly-8.2.0.Final.tar.gz ]
+then echo ""
+else
+	wget http://download.jboss.org/wildfly/8.2.0.Final/wildfly-8.2.0.Final.tar.gz
+	tar -xzf wildfly-8.2.0.Final.tar.gz
 fi
-
-wget http://download.jboss.org/wildfly/8.2.0.Final/wildfly-8.2.0.Final.tar.gz
-tar -xzf wildfly-8.2.0.Final.tar.gz
-
 
 echo "Installing source code from githib repository"
 
-wget https://github.com/waghsk/i2b2-fhir/archive/master.zip 
-unzip master.zip
-#git clone https://github.com/waghsk/i2b2-fhir.git
-
+if [ -f master.zip ]
+then echo ""
+else
+	wget https://github.com/waghsk/i2b2-fhir/archive/master.zip 
+	unzip master.zip
+	git clone https://github.com/waghsk/i2b2-fhir.git
 fi
+
 alias mvn=$MVN
 
 echo "Compiling and deploying war"
