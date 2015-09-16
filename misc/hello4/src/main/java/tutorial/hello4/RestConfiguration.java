3066e6c02e7d693a48305ef31f058c9d647f2092
waghsk <waghsbox@gmail.com>
2015-09-16 12:12:30 -0400

    added license and author info

--- misc/hello4/src/main/java/tutorial/hello4/RestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package tutorial.hello4;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resource")
-public class RestConfiguration extends Application {
-
-}
--- misc/wsclient/src/java/javaeetutorial/helloservice/endpoint/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://hello1.javaeetutorial/")
-package javaeetutorial.helloservice.endpoint;
