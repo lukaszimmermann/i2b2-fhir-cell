d068a8d831679ff7f90889c20b4fa350f1460957
waghsk <waghsbox@gmail.com>
2015-10-03 12:04:41 -0400

    added sep 2015, xjc generated classes. contain resources is broken

--- i2b2-fhir/dstu2/core-2/src/test/java/edu/harvard/i2b2/FhirUtilTest.java
+++ i2b2-fhir/dstu2/core-2/src/test/java/edu/harvard/i2b2/FhirUtilTest.java
@@ -19,7 +19,7 @@ import javax.xml.bind.JAXBException;
 
 import org.apache.commons.io.IOUtils;
 import org.hl7.fhir.Medication;
-import org.hl7.fhir.MedicationPrescription;
+import org.hl7.fhir.MedicationOrder;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,10 +35,10 @@ public class FhirUtilTest {
 	public void contain() throws JAXBException, IOException {
 		// URL path=FhirUtil.class.getResource("validation.zip");
 		// System.out.println(FhirUtil.isValid(Utils.getFile("example/fhir/singlePatient.xml")));
-		String mpXml=IOUtils.toString(FhirUtilTest.class.getResourceAsStream("/example/fhir/DSTU2/singleMedicationPrescription.xml"));
+		String mpXml=IOUtils.toString(FhirUtilTest.class.getResourceAsStream("/example/fhir/DSTU2/singleMedicationOrder.xml"));
 		String mXml=IOUtils.toString(FhirUtilTest.class.getResourceAsStream("/example/fhir/DSTU2/singleMedication.xml"));
 		
-		MedicationPrescription mp= JAXBUtil.fromXml(mpXml,MedicationPrescription.class);
+		MedicationOrder mp= JAXBUtil.fromXml(mpXml,MedicationOrder.class);
 		Medication m= JAXBUtil.fromXml(mXml,Medication.class);
 		
 		//logger.info("MP:"+JAXBUtil.toXml(mp));
