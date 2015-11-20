c00b5fb33f34e38e09e172c4647d69fd4b805331
waghsk <waghsbox@gmail.com>
2015-11-20 17:04:07 -0500

    added logging

--- i2b2-fhir/dstu2/srv-2/src/main/java/edu/harvard/i2b2/oauth2/register/entity/LogEntry.java
+++ i2b2-fhir/dstu2/srv-2/src/main/java/edu/harvard/i2b2/oauth2/register/entity/LogEntry.java
@@ -1,39 +1,112 @@
 package edu.harvard.i2b2.oauth2.register.entity;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 /*
  * fhir cell config from database
  */
 
-@Entity 
+@Entity
 public class LogEntry {
 	@Id
-	String parameter;
+	@GeneratedValue
+	int id;
 	
+	
+	Date dateAccessed;
+
+	String sessionId;
+
+	String accessToken;
+
+	int userId;
+
+	String clientId;
+	String url;
+
 	@Column(columnDefinition = "TEXT")
-	String value;
+	String textComment;
+
 	
+
 	
-	public String getParameter() {
-		return parameter;
+
+	public Date getDateAccessed() {
+		return dateAccessed;
 	}
-	public void setParameter(String parameter) {
-		this.parameter = parameter;
+
+	public void setDateAccessed(Date dateAccessed) {
+		this.dateAccessed = dateAccessed;
 	}
-	public String getValue() {
-		return value;
+
+	public String getSessionId() {
+		return sessionId;
 	}
-	public void setValue(String value) {
-		this.value = value;
+
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
 	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTextComment() {
+		return textComment;
+	}
+
+	public void setTextComment(String textComment) {
+		this.textComment = textComment;
+	}
+
+	public String getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+
 	@Override
 	public String toString() {
-		return "ConfigDb [parameter=" + parameter + ", value=" + value + "]";
+		return "LogEntry [id=" + id + ", dateAccessed=" + dateAccessed + ", sessionId=" + sessionId + ", accessToken="
+				+ accessToken + ", userId=" + userId + ", clientId=" + clientId + ", url=" + url + ", textComment="
+				+ textComment + "]";
 	}
-	
+
 	
 	
 }
