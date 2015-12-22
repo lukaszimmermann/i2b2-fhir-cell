72abe0b6dfa156cf15e303d9fe11076bd5e43725
waghsk <waghsbox@gmail.com>
2015-12-22 18:17:48 -0500

    update script

--- i2b2-fhir/i2b2-fhir-update.sh
+++ i2b2-fhir/i2b2-fhir-update.sh
@@ -13,7 +13,7 @@ git --git-dir="$GITREPO_DIR/.git" pull
 echo "chechking out branch:$BRANCH" 
 git --git-dir="$GITREPO_DIR/.git" checkout $BRANCH
 
-cp -rv "$GITREPO_DIR" "$CELL_DIR/i2b2-fhir-branch"
+cp -r "$GITREPO_DIR" "$CELL_DIR/i2b2-fhir-branch"
 
 #export SA="wildfly-9.0.1.Final/standalone/configuration/standalone.xml" 
 #cat "$CELL_DIR/$SA"|sed s/offset:0/offset:1/>cat "$CELL_DIR/$SA"
