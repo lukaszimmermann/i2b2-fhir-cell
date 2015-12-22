f6693559896f1c34f3192a5445c958008d770b52
waghsk <waghsbox@gmail.com>
2015-12-22 18:10:26 -0500

    update script

--- i2b2-fhir/i2b2-fhir-update.sh
+++ i2b2-fhir/i2b2-fhir-update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#e.g. sh i2b2-fhir-update.sh /exvol/i2b2-fhir-cell /exvol/i2b2-fhir dstu2-sep15-all 1
 #Will update git repository from web and delete old branch from fhir-cell install dir
 #also accomodates branch name and offset for server port in standalone
 export CELL_DIR=$1
@@ -14,7 +13,8 @@ git --git-dir="$GITREPO_DIR/.git" pull
 echo "chechking out branch:$BRANCH" 
 git --git-dir="$GITREPO_DIR/.git" checkout $BRANCH
 
-cp -r "$GITREPO_DIR" "$CELL_DIR/i2b2-fhir-branch"
-export SA="wildfly-9.0.1.Final/standalone/configuration/standalone.xml" 
-cat "$CELL_DIR/$SA"|sed s/offset:0/offset:1/>cat "$CELL_DIR/$SA"
-sh i2b2-fhir-install.sh
+cp -rv "$GITREPO_DIR" "$CELL_DIR/i2b2-fhir-branch"
+
+#export SA="wildfly-9.0.1.Final/standalone/configuration/standalone.xml" 
+#cat "$CELL_DIR/$SA"|sed s/offset:0/offset:1/>cat "$CELL_DIR/$SA"
+sh i2b2-fhir-install.sh $BRANCH
