27dd80b072abd94ebeb250290255d64669eec9f7
waghsk <waghsbox@gmail.com>
2016-02-01 18:12:05 -0500

    fixing httpd conf

--- i2b2-fhir/sudo-install.sh
+++ i2b2-fhir/sudo-install.sh
@@ -1,12 +1,19 @@
-yum -y install git mysqld-server httpd mod_ssl
+timestamp(){
+	date +"%Y-%m-%d_%H-%M-%S" 
+}
+export TS=$(timestamp)
 
-cat i2b2-fhir/install/conf/httpd/append_httpd.conf >> /etc/httpd/conf/httpd.conf
-cat i2b2-fhir/install/conf/httpd/append_httpd.conf >> /etc/httpd/conf.d/ssl.conf
+yum -y install git mysql-server httpd mod_ssl
+
+mv  /etc/httpd/conf/httpd.conf  /etc/httpd/conf/httpd.conf_$TS
+mv  /etc/httpd/conf.d/ssl.conf  /etc/httpd/conf.d/ssl_.conf_$TS
+cp i2b2-fhir/install/conf/httpd/httpd.conf  /etc/httpd/conf/httpd.conf
+cp i2b2-fhir/install/conf/httpd/ssl.conf  /etc/httpd/conf.d/ssl.conf
 
 service httpd start
 service mysqld start
 chkconfig httpd on
 chkconfig mysql on
 
-cat i2b2-fhir/install/conf/db/create_db.sql | mysql -u root
+cat i2b2-fhir/install/conf/db/create_db.sql | /usr/bin/mysql -u root
 
