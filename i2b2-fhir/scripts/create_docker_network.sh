447934964d1aa18e4668c8f471f186531ba5c3e8
waghsk <waghsk@github.com>
2016-04-26 14:37:35 -0400

    created mariadb docker image for fhir cell

---
+++ i2b2-fhir/scripts/create_docker_network.sh
@@ -0,0 +1,12 @@
+BASE=$1
+IP=$2
+MYSQL_PASS=mariai2b2fhir
+sudo docker stop fhir-mariadb; sudo docker rm fhir-mariadb;
+sudo docker rmi -f i2b2/i2b2-mariadb
+
+docker build -t i2b2/i2b2-mariadb $BASE/install/conf/docker/maria-db/;
+docker run --name=fhir-mariadb -d -p 3306:3306  -e "MYSQL_ROOT_PASSWORD=$MYSQL_PASS" i2b2/i2b2-mariadb
+sleep 5;
+sudo docker exec -it fhir-mariadb bash -c "/i2b2fhir_db.sh $MYSQL_PASS; "
+exit
+
